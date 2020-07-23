dca7e4c0b5a6f7fbbe793c7cff6591534affc92d
wilhamsJW <wilhamsmeira@gmail.com>
2020-07-23 17:11:04 -0300

    Adding file index

---
+++ Frontend/includes/form_inc.php
@@ -0,0 +1,35 @@
+<div class="container py-5" style="margin-top: 40px;">
+    <div class="container py-5">
+        <div class="col-md-6 container clearfix">
+            <fieldset id="formu">
+                <form action="banco_de_dados/create.php" method="POST" class="py-">
+                    <fieldset class="formulario">
+                        <h1 class="text-center" id="font">Cadastro</h1>
+                        <div class="form-group row" style="margin-top: 30px">
+                            <label for="inputEmail3" id="font" class="col-sm-2 col-form-label">Nome</label>
+                            <div class="col-sm-10">
+                                <input type="name" name="name" id="inpu" class="form-control" placeholder="Nome do usuário">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputEmail3" id="font" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" name="email" class="form-control" id="inputEmail3" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputPassword3" id="font" class="col-sm-2 col-form-label">Senha</label>
+                            <div class="col-sm-10">
+                                <input type="password" name="senha2" class="form-control" id="inputPassword3" placeholder="Senha">
+                            </div>
+                        </div>
+                        <div style="text-align: right;">
+                            <input class="btn" id="button" type="submit" value="Cadastrar">
+                        </div>
+                    </fieldset>
+                </form>
+            </fieldset>
+        </div>
+    </div>
+</div>
+</span>
--- Frontend/includes/header_inc.php
+++ Frontend/includes/header_inc.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
 
-    <title>Olá, mundo!</title>
+    <title>Servicenet</title>
   </head>
   <body>
