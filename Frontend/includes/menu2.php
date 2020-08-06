447d8b99379202cf06a1d177d47286a79543bafa
wilhamsJW <wilhamsmeira@gmail.com>
2020-08-05 22:55:01 -0300

    fixing mobile menu

--- Frontend/includes/menu2.php
+++ Frontend/includes/menu2.php
@@ -5,7 +5,7 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse">
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav ml-auto">
                     
                     <li class="nav-item active">
